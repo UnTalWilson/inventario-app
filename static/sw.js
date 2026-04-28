28c43934b264e34cc2ae46810c0d6746d3caff0c
Wilson <lokendo420@gmail.com>
2026-04-27 22:24:07 -0400

    agregar PWA manifest y service worker

--- static/sw.js
+++ static/sw.js
@@ -1,1 +1,21 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'inventario-v1';
+const urlsToCache = [
+  '/',
+  '/inicio',
+  '/static/manifest.json',
+  '/static/icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
